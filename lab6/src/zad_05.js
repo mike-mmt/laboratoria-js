029f69b044da736e75fbd6d74c8ff2c815ed72f0
Michał Bunda <m.bunda.721@studms.ug.edu.pl>
2023-04-03 19:41:46 +0200

    lab6 zad_05

--- lab6/src/zad_05.js
+++ lab6/src/zad_05.js
@@ -8,14 +8,44 @@ const miniseries = seriesData.filter(series => series.seasons === 1).reduce( (ac
     return [...acc, formattedSeries];
 }, []).sort();
 
-const series = seriesData.reduce( (wholeArray, currentSeries) => {
+const series = seriesData.filter(series => series.seasons !== 1).reduce( (wholeArray, currentSeries) => {
     if (currentSeries.startYear < 2010) {
         //
-        // const formattedSeries = {};
-    } else if (2010 <= currentSeries.startYear < 2020) {
-        //
+        const formattedSeries = {};
+        formattedSeries["id"] = currentSeries.id;
+        formattedSeries["name"] = currentSeries.name;
+        formattedSeries["type"] = currentSeries.type.reduce( (typeAcc, curType, typeIndex) => `${typeAcc}${typeIndex === 0 ? '' : ', '}${curType}`, '');
+        formattedSeries["startYear"] = currentSeries.startYear;
+        if (currentSeries["endYear"] !== null) {
+            formattedSeries["endYear"] = currentSeries.endYear;
+        }
+        formattedSeries["seasons"] = currentSeries.seasons;
+        wholeArray[0].push(formattedSeries);
+        return wholeArray;
+    } else if (currentSeries.startYear >= 2010 && currentSeries.startYear < 2020) {
+        const formattedSeries = {};
+        formattedSeries["id"] = currentSeries.id;
+        formattedSeries["name"] = currentSeries.name;
+        formattedSeries["type"] = currentSeries.type.reduce( (typeAcc, curType, typeIndex) => `${typeAcc}${typeIndex === 0 ? '' : ', '}${curType}`, '');
+        formattedSeries["startYear"] = currentSeries.startYear;
+        if (currentSeries["endYear"] !== null) {
+            formattedSeries["endYear"] = currentSeries.endYear;
+        }
+        formattedSeries["seasons"] = currentSeries.seasons;
+        wholeArray[1].push(formattedSeries);
+        return wholeArray;
     } else {
-        //
+        const formattedSeries = {};
+        formattedSeries["id"] = currentSeries.id;
+        formattedSeries["name"] = currentSeries.name;
+        formattedSeries["type"] = currentSeries.type.reduce( (typeAcc, curType, typeIndex) => `${typeAcc}${typeIndex === 0 ? '' : ', '}${curType}`, '');
+        formattedSeries["startYear"] = currentSeries.startYear;
+        if (currentSeries["endYear"] !== null) {
+            formattedSeries["endYear"] = currentSeries.endYear;
+        }
+        formattedSeries["seasons"] = currentSeries.seasons;
+        wholeArray[2].push(formattedSeries);
+        return wholeArray;
     }
 }, [[], [], []]);
 
