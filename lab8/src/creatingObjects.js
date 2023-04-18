da2030090504bb5f340e107e199030c056870d7b
mbunda <m.bunda.721@studms.ug.edu.pl>
2023-04-18 15:10:15 +0200

    lab8 zad 1 - 2

---
+++ lab8/src/creatingObjects.js
@@ -0,0 +1,123 @@
+﻿// W poniższych zadaniach zostało przedstawione tworzenie obiektów na różne sposoby (czyli, jak osiągnąć to samo pisząc różny kod).
+
+// W każdym poniższym obiekcie zdefiniuj 
+// - pola 'title' i 'author'
+// - funkcję print(), która wypisze: "Autorem ... jest ..."
+
+// WAŻNE
+// Przy deklaracji funkcji w obiektach _NIE_ używaj funkcji strzałkowych
+
+
+// ------------
+// Sposób 1.
+// ------------
+
+const book1 = {
+  // properties
+  title: "Romeo i Julia",
+  author: "William Szekspir",
+  print: function () {
+    console.log(`Autorem '${this.title}' jest ${this.author}`);
+  }
+};
+
+book1.print();
+
+
+// ------------
+// Sposób 2.
+// ------------
+
+const book2 = {};
+// properties
+book2["title"] = "Odyseja";
+book2["author"] = "Homer";
+book2["print"] = function () {
+  console.log(`Autorem '${this.title}' jest ${this.author}`);
+}
+book2.print();
+
+
+// ------------
+// Sposób 3.
+// ------------
+
+function createBook(title, author) {
+  const b = {};
+  b["title"] = title,
+  b["author"] = author,
+  b["print"] = function () {
+    console.log(`Autorem '${this.title}' jest ${this.author}`);
+  }
+  return b
+  // properties
+}
+
+const book3 = createBook("Cień wiatru", "Carlos Ruiz Zafon");
+const book4 = createBook("Ojciech Chrzestny", "Mario Puzo");
+
+book3.print();
+book4.print();
+
+
+// ------------
+// Sposób 4.
+// ------------
+
+// Stwórz funkcję Book tak, aby inicjalizowała pola author i title (użyj operatora this)
+
+function Book(title, author) {
+  // Uzupełnij
+  this.title = title;
+  this.author = author;
+  this.readers = 0;
+  this.addReader = function () {
+    this.readers++;
+  }
+  this.printReaders = function () {
+    console.log(`Ilość czytelników: ${this.readers}`);
+  }
+}
+
+const book5 = new Book("Ojciech Chrzestny", "Mario Puzo");
+
+// Definiując obiekt w tym przypadku używamy słówka "new", co oznacza, że funkcja jest konstruktorem. Konstruktory ZAWSZE deklarujemy zaczynając nazwę od wielkiej litery. Przyjęło się konwencje, że nazwa powinna być nazwą obiektu, który tworzymy (w tym przypadku tworzymy książkę).
+
+
+// Dopisz w tworzonym obiekcie: 
+// - pole readers, które będzie zawierało liczbę czytelników (na starcie przyjmujące wartość 0).
+// - funkcję addReader, która inkrementuje pole readers (addReader powinna być funkcją wspólną, tak jak print)
+// - funkcję printReaders wypisującą ilość czytelników
+
+book5.printReaders();
+book5.addReader();
+book5.printReaders();
+
+
+// ------------
+// Sposób 5.
+// ------------
+
+// Tworzymy alternatywną wersję powyższego kodu. Użyj słów kluczowych class i constructor, aby osiągnąć ten sam powyższy efekt. 
+// Żeby nie zakomentowywać powyższego kodu, możesz nazwać klasę BookCreator
+
+class BookCreator {
+  // Uzupełnij
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+  readers = 0;
+  addReader = function () {
+    this.readers++;
+  }
+  printReaders = function () {
+    console.log(`Ilość czytelników: ${this.readers}`);
+  }
+}
+
+const book6 = new BookCreator("Ojciech Chrzestny", "Mario Puzo");
+
+book6.printReaders();
+book6.addReader();
+book6.printReaders();
---
+++ lab8/src/objects.js
@@ -0,0 +1,118 @@
+﻿'use strict';
+
+// ==============================================
+// Przeanalizuj poniższe fragmenty kodu i zastanów się, co zostanie wypisane na ekranie.
+// ==============================================
+
+//#region EXAMPLES
+
+// Przykład 1
+
+const arr = ['Pies', 'Kot', 'Królik', 'Papuga', 'Małpa'];
+const [pies, kot, krolik, papuga, malpa] = arr;
+
+// console.log(pies, krolik); // Pies Królik
+
+// Przykład 2
+
+const book = {
+  title: 'Mistrz i Małgorzata',
+  author: 'Michaił Bułhakow',
+  year: 1967
+};
+
+// a
+
+const { title, author, year } = book;
+
+// console.log(title);  // Mistrz i Małgorzata
+// console.log(author); // Michaił Bułhakow
+
+// b
+
+const {
+  title: titleOfBook,
+  author: authorOfBook,
+  year: yearOfBook
+} = book;
+
+// console.log(titleOfBook); // Mistrz i Małgorzata
+// console.log(authorOfBook); // Michaił Bułhakow
+
+// c
+
+const { title: onlyTitle, ...bookWithoutTitle } = book;
+
+// console.log(onlyTitle); // Mistrz i Małgorzata
+// console.log(bookWithoutTitle); // { author: ..., year: ... }
+
+
+//#endregion EXAMPLES
+
+
+// ==============================================
+// ZADANIA
+// ==============================================
+
+// Zadanie 1
+
+// Wykorzystując wszystkie informacje zawarte powyżej stwórz obiekt zawierający poniższe pola:
+// country: USA
+// title: Zielona Mila
+// director: Frank Darabont
+// genre: Dramat
+
+const movie1 = {
+  country: 'USA',
+  title: 'Zielona Mila',
+  director: 'Frank Darabont',
+  genre: 'Dramat'
+};
+
+// Następnie wykorzystując zabieg przedstawiony w powyższych przykładach zmodyfikuj obiekt tak, aby po wyświetleniu obiektu dostać następujący output:
+
+const {
+  country,
+  genre,
+  ...newMovie1
+} = movie1;
+
+console.log(newMovie1);
+// Oczekiwany output: { title: 'Zielona Mila', director: 'Frank Darabont' }
+
+
+// Zadanie 2
+
+// Stwórz teraz bardziej skomplikowany obiekt zawierający m.in tablicę i inny obiekt. Powiedzmy, że zawiera np.
+// title: Nietykalni
+// directors: Olivier Nakache, Éric Toledano
+// country: Francja
+// year: 2011
+// actors: Philippe: François Cluzet, Dris: Omar Sy, Yvonne: Anne Le Ny
+
+const movie2 = {
+  title: 'Nietykalni',
+directors: ['Olivier Nakache', 'Éric Toledano'],
+country: 'Francja',
+year: 2011,
+actors: { Philippe: 'François Cluzet', Dris: 'Omar Sy', Yvonne: 'Anne Le Ny' }
+};
+
+// Następnie odwzoruj wygląd stworzonej struktury obiektu i wykonaj na nim poniższe operacje
+
+const {
+  title: titleOfMovie,
+  directors: [director1, director2],
+  actors: {
+    Philippe: philippeRole,
+    Dris: drisRole,
+    Yvonne: yvonneRole
+  } 
+} = movie2;
+
+console.log(titleOfMovie); // Oczekiwany output -> Nietykalni
+console.log(director1); // Oczekiwany output -> Olivier Nakache
+console.log(director2); // Oczekiwany output -> Éric Toledano
+console.log(philippeRole); // Oczekiwany output -> François Cluzet
+console.log(drisRole); // Oczekiwany output -> Omar Sy
+console.log(yvonneRole); // Oczekiwany output -> Anne Le Ny
